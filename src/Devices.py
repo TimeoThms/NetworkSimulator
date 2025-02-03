f07a94da91b8e640e90e24518bd37140ade4a5e7
TimeoThms <timeo.thomas35@gmail.com>
2025-02-03 17:34:27 +0100

    Packet routing improvements

--- src/Devices.py
+++ src/Devices.py
@@ -7,10 +7,9 @@ class Device:
     def __init__(self, name: str, interfaces: dict[str, Interface] = None):
         self.name = name
         self.interfaces = interfaces
-        # self.arp_table = {}
 
     def arp_request(self, interface, target_ip):
-        """Returns the MAC Adress of in interface with the specified IPv4 on the same subnet"""
+        """Returns the MAC Address of in interface with the specified IPv4 on the same subnet"""
         if target_ip in self.interfaces[interface.name].arp_table:
             return self.interfaces[interface.name].arp_table[target_ip]
 
@@ -31,11 +30,27 @@ class Device:
                 return response_mac
 
     def arp_reply(self, target_ip: str):
-        """Returns MAC adress if target_ip is and IPv4 of an interface on the Device"""
+        """Returns MAC address if target_ip is and IPv4 of an interface on the Device"""
         for interface in self.interfaces.values():
             if interface.ipv4 == target_ip:
                 return interface.mac
         return None
+
+    def handle_packet(self, packet: Packet):
+        if packet.protocol != "ARP":
+            for interface in self.interfaces.values():
+                if packet.dest_mac == interface.mac:
+                    print(f"{self.name} - Received packet from {packet.src_ip} on interface {interface.name}")
+                    return
+                if isinstance(self, Router):
+                    self.transmit_packet(packet)
+                    return
+                if isinstance(self, Switch):
+                    self.forward_packet(packet, interface)
+                    return
+        else:
+            pass
+
 
 
 class Router(Device):
@@ -51,9 +66,11 @@ class Router(Device):
 
                 interface = self.routing_table[network]["interface"]
                 self.arp_request(interface, packet.dest_ip)
-                destination_mac = self.interfaces[interface.name].arp_table
+                destination_mac = self.interfaces[interface.name].arp_table[packet.dest_ip]
                 if destination_mac:
                     print(f"Packet sent to {packet.dest_ip} via interface {interface.name} and MAC {destination_mac}")
+                    packet.dest_mac = destination_mac
+                    interface.connected_to.device.handle_packet(packet)
                     return
 
     def get_mac_from_arp_table(self, target_ip, interface):
@@ -63,7 +80,7 @@ class Router(Device):
 
 
 class Switch(Device):
-    def __init__(self, name: str, num_ports: int, interfaces: dict[str, Interface] = None):
+    def __init__(self, name: str, num_ports: int):
         interfaces = {f"eth{i}": Interface(f"eth{i}", self) for i in range(num_ports)}
         super().__init__(name, interfaces)
 
@@ -76,6 +93,22 @@ class Switch(Device):
                     return response
         return None
 
+    def forward_packet(self, packet: Packet, src_iface: Interface):
+        """Forward a Packet through the Switch"""
+        for interface in self.interfaces.values():
+            if interface.connected_to and src_iface != interface and interface.connected_to.mac == packet.dest_mac:
+                print(f"{self.name} - Packet forwarded directly to destination ({packet.dest_ip}) via interface {interface.name}")
+                interface.connected_to.device.handle_packet(packet)
+                return
+        else:
+            # Forward packet to all ports
+            for interface in self.interfaces.values():
+                print(f"{self.name} - Forwarding packet to all ports...")
+                if interface.connected_to and src_iface != interface:
+                    print(f"Forwarding to {interface.name}")
+                    interface.connected_to.device.handle_packet(packet)
+            return
+
 
 class Computer(Device):
     def __init__(self, name: str, gateway: str):
@@ -84,21 +117,27 @@ class Computer(Device):
         self.gateway = gateway
 
     def send_packet(self, packet: Packet):
-        """Sends a packet to the specified IPv4 adress"""
+        """Sends a packet to the specified IPv4 address"""
         # Handle packets if the source and destination are on the same subnet
         for interface in self.interfaces.values():
             if interface.ipv4 and IPv4(interface.ipv4).get_subnet() == IPv4(packet.dest_ip).get_subnet():
-                print(f"Same subnet via {interface.name}")
+                #print(f"Same subnet via {interface.name}")
+                if packet.dest_ip in interface.arp_table.keys():
+                    packet.dest_mac = interface.arp_table[packet.dest_ip]
+                else:
+                    self.arp_request(interface, packet.dest_ip)
+                    if packet.dest_ip in interface.arp_table.keys():
+                        packet.dest_mac = interface.arp_table[packet.dest_ip]
+                    else:
+                        print(f"{self.name} - Unable to resolve destination MAC Address")
+                        return
+                interface.connected_to.device.handle_packet(packet)
                 return
         # If the destination is not on the same subnet, use the default gateway
         for interface in self.interfaces.values():
             if interface.ipv4 and self.gateway and IPv4(interface.ipv4).get_subnet() == IPv4(
                     self.gateway).get_subnet():
                 if interface.connected_to and interface.connected_to.ipv4 == self.gateway:
-                    if isinstance(interface.connected_to.device, Router):
-                        interface.connected_to.device.transmit_packet(packet)
-                        return
-                    else:
-                        print("Gateway is not a Router!")
+                    interface.connected_to.device.handle_packet(packet)
                 else:
                     print("Unable to connect to the default gateway")
--- src/network.py
+++ src/network.py
@@ -6,8 +6,8 @@ from Packet import Packet
 pc1 = Computer("PC-01", "192.168.1.254")
 pc1.interfaces["eth0"].ipv4 = "192.168.1.1"
 
-pc2 = Computer("PC-02", "172.16.0.254")
-pc2.interfaces["eth0"].ipv4 = "172.16.0.1"
+pc2 = Computer("PC-02", "192.168.1.254")
+pc2.interfaces["eth0"].ipv4 = "192.168.1.2"
 
 # Switchs
 s1 = Switch("Switch 1", 4)
@@ -19,13 +19,13 @@ r1.interfaces["eth1"].ipv4 = "172.16.0.254"
 
 
 # Interfaces
-pc1.interfaces["eth0"].connect(r1.interfaces["eth0"], LinkType.ETHERNET)
-# pc2.interfaces["eth0"].connect(s1.interfaces["eth1"], LinkType.ETHERNET)
-pc2.interfaces["eth0"].connect(r1.interfaces["eth1"], LinkType.FASTETHERNET)
+pc1.interfaces["eth0"].connect(s1.interfaces["eth0"], LinkType.ETHERNET)
+pc2.interfaces["eth0"].connect(s1.interfaces["eth1"], LinkType.ETHERNET)
+#pc2.interfaces["eth0"].connect(r1.interfaces["eth1"], LinkType.FASTETHERNET)
 
 r1.routing_table = {
     "192.168.1.0": {"interface": r1.interfaces["eth0"], "metric": 0},
     "172.16.0.0": {"interface": r1.interfaces["eth1"], "metric": 0}
 }
 
-pc1.send_packet(Packet("192.168.1.1", "172.16.0.1", "Hello"))
+pc1.send_packet(Packet("192.168.1.1", "192.168.1.2", "Hello"))
